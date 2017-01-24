2988ee1798120f86134d1374c65ca828916684de
Ben Iofel <iofelben@gmail.com>
2017-01-23 21:31:05 -0500

    add jnlp sandbox script

---
+++ run-jnlp-sandbox.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+function die {
+    echo $@
+    exit
+}
+
+[[ $# -eq 1 ]] || die "usage: $0 <jnlp file>"
+
+[[ -f $1 ]] || die "no such file \`$1'"
+
+cp $1 /tmp
+cd /tmp
+firejail --seccomp --private javaws /tmp/$(basename $1)
+rm /tmp/$(basename $1)

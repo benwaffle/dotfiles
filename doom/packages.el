14ec46bd7e75bd23ae9d6053028660751eecd1a0
Ben Iofel <ben@flow.io>
2022-11-11 12:42:16 +0000

    Simplify ement.el install

--- doom/packages.el
+++ doom/packages.el
@@ -11,12 +11,7 @@
 
 (package! jenkinsfile-mode)
 
-;; Install `plz' HTTP library (not on MELPA yet).
-(package! plz
-  :recipe (:host github :repo "alphapapa/plz.el"))
-;; Install Ement.
-(package! ement
-  :recipe (:host github :repo "alphapapa/ement.el"))
+(package! ement)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:

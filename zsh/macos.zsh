8373420f5040382acab0f13296d904ee25530f4b
Ben Iofel <bi@fly.io>
2023-08-25 20:41:50 -0400

    delete old flow stuff

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -1,6 +1,5 @@
 export HOMEBREW_NO_ANALYTICS=1
 export PKG_CONFIG_PATH="/usr/local/opt/libffi/lib/pkgconfig"
-export GOPRIVATE=github.com/flowcommerce
 
 zi wait pack for brew-completions
 
@@ -10,26 +9,8 @@ export PATH="/opt/homebrew/opt/python@3/libexec/bin:/Users/ben/Library/Applicati
 alias -s app='open -a' # run .app files just like executables
 
 alias bup='brew update && brew upgrade && brew upgrade --cask && brew cleanup'
-#alias stern='stern --exclude-container=istio-proxy --exclude _internal_/healthcheck'
-alias logs='stern --exclude _internal_/healthcheck --tail 50'
-alias nodes="kgno -L 'role,node.kubernetes.io/instance-type,topology.kubernetes.io/zone' --sort-by '{.metadata.creationTimestamp}'"
-
-alias helm3=/opt/homebrew/opt/helm@3/bin/helm
-alias helm2=/opt/homebrew/opt/helm@2/bin/helm
-alias helm='echo specify helm2 or helm3 #'
 
 alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
-
-hc() {
-    echo https://$1.api.flow.io/_internal_/healthcheck
-    curl -v https://$1.api.flow.io/_internal_/healthcheck
-}
-
-kpv() {
-    kubectl get pods -l app=$1 -n production -L flow.io/version
-}
-
-# [ -e ~/.travis/travis.sh ] && . ~/.travis/travis.sh
 
 [ -e ~/.iterm2_shell_integration.zsh ] && . ~/.iterm2_shell_integration.zsh
 
